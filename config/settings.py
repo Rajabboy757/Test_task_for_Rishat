79045285478bf4f14896b346226cff9efbefdcb5
Rajabboy <rajrajrajradju@gmail.com>
2023-02-16 12:59:19 +0500

    installed swagger and fixed some bugs

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
     # external apps
     'rest_framework',
+    'drf_yasg',
 
     # local apps
     'payments',
@@ -114,7 +115,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import TemplateView, DetailView, ListView
+from django.shortcuts import render
+from django.views.generic import ListView
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-from rest_framework.views import APIView
 from .serializers import ItemSerializer
 from .models import Items
 from rest_framework.response import Response
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 class ItemView(generics.GenericAPIView):
     serializer_class = ItemSerializer
     renderer_classes = (JSONRenderer, TemplateHTMLRenderer,)
-    queryset = Items.objects.all()
 
     def get(self, request, *args, **kwargs):
         pk = kwargs.get('pk', '')
@@ -34,7 +33,7 @@ class BuyView(generics.GenericAPIView):
         if request.accepted_renderer.format == 'html':
             return Response(data, template_name='buy.html')
 
-        return Response(data)
+        return render(request, 'buy.html')
 
 
 class ItemsListView(ListView):
